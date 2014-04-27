c3f1452d481d71a677fc1e0473035cfe6951d611
Matt VanNuys <vannuysm@gmail.com>
2014-04-27 04:17:43 -0400

    Added call to update migrations on app start

--- Leaguerly.Api/Global.asax.cs
+++ Leaguerly.Api/Global.asax.cs
@@ -1,10 +1,17 @@
-﻿using System.Web.Http;
+﻿using System.Data.Entity.Migrations;
+using System.Web.Http;
 
 namespace Leaguerly.Api
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
         protected void Application_Start() {
+            
+            // Make sure we run migrations
+            var migrationConfig = new Migrations.Configuration();
+            var dbMigrator = new DbMigrator(migrationConfig);
+            dbMigrator.Update();
+
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
